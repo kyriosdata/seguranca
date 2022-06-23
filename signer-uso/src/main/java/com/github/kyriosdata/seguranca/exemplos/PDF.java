1669fe9c24842db2d71c7fbf4c29e5e68d5050d5
kyriosdata <fabio@inf.ufg.br>
2022-06-23 10:04:00 -0300

    ajustes

--- signer-uso/src/main/java/com/github/kyriosdata/seguranca/exemplos/PDF.java
+++ signer-uso/src/main/java/com/github/kyriosdata/seguranca/exemplos/PDF.java
@@ -8,8 +8,24 @@ import org.apache.pdfbox.io.RandomAccessReadBufferedFile;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.encryption.SecurityProvider;
+import org.apache.pdfbox.pdmodel.interactive.digitalsignature.COSFilterInputStream;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.PDSignature;
 import org.apache.pdfbox.util.Hex;
+import org.bouncycastle.asn1.cms.Attribute;
+import org.bouncycastle.asn1.cms.CMSAttributes;
+import org.bouncycastle.asn1.cms.Time;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.cms.CMSProcessable;
+import org.bouncycastle.cms.CMSSignedData;
+import org.bouncycastle.cms.SignerInformation;
+import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.tsp.TSPException;
+import org.bouncycastle.tsp.TimeStampToken;
+import org.bouncycastle.util.Store;
 import org.demoiselle.signer.cryptography.DigestAlgorithmEnum;
 import org.demoiselle.signer.policy.impl.cades.SignatureInformations;
 import org.demoiselle.signer.policy.impl.cades.SignerAlgorithmEnum;
@@ -20,9 +36,18 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.security.cert.CertificateExpiredException;
+import java.security.cert.CertificateNotYetValidException;
+import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -36,12 +61,14 @@ public final class PDF {
      * @param args Caminho do arquivo PDF cuja assinatura será extraída.
      * @throws IOException If there is an error reading the file.
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, OperatorCreationException, GeneralSecurityException, TSPException, CMSException {
+        Security.setProperty("crypto.policy", "unlimited");
+
         PDF show = new PDF();
         show.extraiAssinatura("d:/downloads/assinado.pdf");
     }
 
-    private void extraiAssinatura(String arquivo) throws IOException {
+    private void extraiAssinatura(String arquivo) throws IOException, OperatorCreationException, GeneralSecurityException, TSPException, CMSException {
         System.out.println("Arquivo a ser analisado: " + arquivo);
         String password = "";
         File infile = new File(arquivo);
@@ -58,6 +85,7 @@ public final class PDF {
             System.out.println("Assinatura presente no arquivo.");
             System.out.println("Total de assinaturas: " + signatureDictionaries.size());
             for (PDSignature sig : signatureDictionaries) {
+
                 byte[] contents = sig.getContents();
 
                 Path path = Paths.get(arquivo + ".p7s");
@@ -77,6 +105,15 @@ public final class PDF {
                     System.out.println("Subfilter não encontrado?... ");
                     return;
                 }
+
+                FileInputStream fis = new FileInputStream(infile);
+                InputStream signedContentAsStream = new COSFilterInputStream(fis, sig.getByteRange());
+
+                // Just to generate file content that was signed
+//                Path signed = Paths.get(arquivo + ".signed");
+//                Files.copy(signedContentAsStream, signed, StandardCopyOption.REPLACE_EXISTING);
+
+                verifyPKCS7(signedContentAsStream, contents, sig);
 
                 int[] byteRange = sig.getByteRange();
                 if (byteRange.length != 4) {
@@ -160,10 +197,11 @@ public final class PDF {
                 e.printStackTrace();
             }
 
-            byte[] hash = md.digest(contentFromFile);
+            Path signed = Paths.get("d:/downloads/assinado.pdf.signed");
+            byte[] hash = md.digest(Files.readAllBytes(signed));
 
             List<SignatureInformations> signaturesInfo = checker.checkSignatureByHash(SignerAlgorithmEnum.SHA256withRSA.getOIDAlgorithmHash(), hash, contents);
-
+            System.out.println(signaturesInfo.size());
         }
     }
 
@@ -216,4 +254,119 @@ public final class PDF {
             }
         }
     }
+
+    /**
+     * Verify a PKCS7 signature.
+     *
+     * @param signedContentAsStream the byte sequence that has been signed
+     * @param contents              the /Contents field as a COSString
+     * @param sig                   the PDF signature (the /V dictionary)
+     * @throws CMSException
+     * @throws OperatorCreationException
+     * @throws GeneralSecurityException
+     */
+    private void verifyPKCS7(InputStream signedContentAsStream, byte[] contents, PDSignature sig)
+            throws CMSException, OperatorCreationException,
+            GeneralSecurityException,
+            TSPException, IOException {
+        // inspiration:
+        // http://stackoverflow.com/a/26702631/535646
+        // http://stackoverflow.com/a/9261365/535646
+        CMSProcessable signedContent = new CMSProcessableInputStream(signedContentAsStream);
+        CMSSignedData signedData = new CMSSignedData(signedContent, contents);
+        Store<X509CertificateHolder> certificatesStore = signedData.getCertificates();
+        if (certificatesStore.getMatches(null).isEmpty()) {
+            throw new IOException("No certificates in signature");
+        }
+        Collection<SignerInformation> signers = signedData.getSignerInfos().getSigners();
+        if (signers.isEmpty()) {
+            throw new IOException("No signers in signature");
+        }
+        SignerInformation signerInformation = signers.iterator().next();
+        @SuppressWarnings("unchecked")
+        Collection<X509CertificateHolder> matches =
+                certificatesStore.getMatches(signerInformation.getSID());
+        if (matches.isEmpty()) {
+            throw new IOException("Signer '" + signerInformation.getSID().getIssuer() +
+                    ", serial# " + signerInformation.getSID().getSerialNumber() +
+                    " does not match any certificates");
+        }
+        X509CertificateHolder certificateHolder = matches.iterator().next();
+        X509Certificate certFromSignedData = new JcaX509CertificateConverter().getCertificate(certificateHolder);
+        System.out.println("certFromSignedData: " + certFromSignedData);
+
+        SigUtils.checkCertificateUsage(certFromSignedData);
+
+        // Embedded timestamp
+        TimeStampToken timeStampToken = SigUtils.extractTimeStampTokenFromSignerInformation(signerInformation);
+        if (timeStampToken != null) {
+            // tested with QV_RCA1_RCA3_CPCPS_V4_11.pdf
+            // https://www.quovadisglobal.com/~/media/Files/Repository/QV_RCA1_RCA3_CPCPS_V4_11.ashx
+            // also 021496.pdf and 036351.pdf from digitalcorpora
+            SigUtils.validateTimestampToken(timeStampToken);
+            X509Certificate certFromTimeStamp = SigUtils.getCertificateFromTimeStampToken(timeStampToken);
+            // merge both stores using a set to remove duplicates
+            HashSet<X509CertificateHolder> certificateHolderSet = new HashSet<>();
+            certificateHolderSet.addAll(certificatesStore.getMatches(null));
+            certificateHolderSet.addAll(timeStampToken.getCertificates().getMatches(null));
+
+            SigUtils.checkTimeStampCertificateUsage(certFromTimeStamp);
+
+            // compare the hash of the signature with the hash in the timestamp
+            byte[] tsMessageImprintDigest = timeStampToken.getTimeStampInfo().getMessageImprintDigest();
+            String hashAlgorithm = timeStampToken.getTimeStampInfo().getMessageImprintAlgOID().getId();
+            byte[] sigMessageImprintDigest = MessageDigest.getInstance(hashAlgorithm).digest(signerInformation.getSignature());
+            if (Arrays.equals(tsMessageImprintDigest, sigMessageImprintDigest)) {
+                System.out.println("timestamp signature verified");
+            } else {
+                System.err.println("timestamp signature verification failed");
+            }
+        }
+
+        try {
+            if (sig.getSignDate() != null) {
+                certFromSignedData.checkValidity(sig.getSignDate().getTime());
+                System.out.println("Certificate valid at signing time");
+            } else {
+                System.err.println("Certificate cannot be verified without signing time");
+            }
+        } catch (CertificateExpiredException ex) {
+            System.err.println("Certificate expired at signing time");
+        } catch (CertificateNotYetValidException ex) {
+            System.err.println("Certificate not yet valid at signing time");
+        }
+
+        // usually not available
+        if (signerInformation.getSignedAttributes() != null) {
+            // From SignedMailValidator.getSignatureTime()
+            Attribute signingTime = signerInformation.getSignedAttributes().get(CMSAttributes.signingTime);
+            if (signingTime != null) {
+                Time timeInstance = Time.getInstance(signingTime.getAttrValues().getObjectAt(0));
+                try {
+                    certFromSignedData.checkValidity(timeInstance.getDate());
+                    System.out.println("Certificate valid at signing time: " + timeInstance.getDate());
+                } catch (CertificateExpiredException ex) {
+                    System.err.println("Certificate expired at signing time");
+                } catch (CertificateNotYetValidException ex) {
+                    System.err.println("Certificate not yet valid at signing time");
+                }
+            }
+        }
+
+        if (signerInformation.verify(new JcaSimpleSignerInfoVerifierBuilder().
+                setProvider(SecurityProvider.getProvider()).build(certFromSignedData))) {
+            System.out.println("Signature verified");
+        } else {
+            System.out.println("Signature verification failed");
+        }
+
+        System.out.println("Certificate is not self-signed");
+
+        if (sig.getSignDate() != null) {
+            System.out.println("it has sign date");
+        } else {
+            System.err.println("Certificate cannot be verified without signing time");
+        }
+    }
+
 }
